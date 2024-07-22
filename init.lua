5bcc6151daf927cfe040686d460e26c782c7aa8a
user <blessdarahuba@gmail.com>
2024-07-22 21:37:17 +0100

    chore: update plugins

--- init.lua
+++ init.lua
@@ -2,7 +2,3 @@
 --require("lua.config.keymaps")
 require("lazy-vim")
 vim.cmd("colorscheme nord")
-vim.api.nvim_create_autocmd("BufWritePre", {
-	pattern = "*.php",
-	command = "silent !blade-formatter --write resources/**/*.blade.php",
-})
