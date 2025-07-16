22f556c1691649d94bb2c006c393c3da40bf303d
Bless Darah <bless.darah@cm.maviance.com>
2025-07-16 12:45:36 +0100

    Ignore

--- lua/autoload/settings.lua
+++ lua/autoload/settings.lua
@@ -31,6 +31,9 @@ opt.smartcase = true
 -- cursor line
 opt.cursorline = true
 
+-- cursor shape and appearance
+opt.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
+
 -- Appearance
 opt.termguicolors = true
 opt.background = "dark"
--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -21,6 +21,69 @@ return {
 				-- show_hidden = true, -- Show hidden files
 				show_git_status = true, -- Enable Git status integration
 			},
+			keymaps = {
+				["g?"] = "actions.show_help",
+				["<CR>"] = "actions.select",
+				["<C-s>"] = "actions.select_vsplit",
+				["<C-h>"] = "actions.select_split",
+				["<C-t>"] = "actions.select_tab",
+				["<C-p>"] = "actions.preview",
+				["<C-c>"] = "actions.close",
+				["<C-l>"] = "actions.refresh",
+				["-"] = "actions.parent",
+				["_"] = "actions.open_cwd",
+				["`"] = "actions.cd",
+				["~"] = "actions.tcd",
+				["gs"] = "actions.change_sort",
+				["gx"] = "actions.open_external",
+				["g."] = "actions.toggle_hidden",
+				["g\\"] = "actions.toggle_trash",
+				-- Git operations
+				["<leader>gs"] = {
+					function()
+						local oil = require("oil")
+						local filename = oil.get_cursor_entry().name
+						if filename then
+							vim.cmd("!git add " .. vim.fn.shellescape(filename))
+							oil.refresh()
+						end
+					end,
+					desc = "Git stage file",
+				},
+				["<leader>gu"] = {
+					function()
+						local oil = require("oil")
+						local filename = oil.get_cursor_entry().name
+						if filename then
+							vim.cmd("!git reset HEAD " .. vim.fn.shellescape(filename))
+							oil.refresh()
+						end
+					end,
+					desc = "Git unstage file",
+				},
+				["<leader>gd"] = {
+					function()
+						local oil = require("oil")
+						local filename = oil.get_cursor_entry().name
+						if filename then
+							vim.cmd("vertical Git diff " .. vim.fn.shellescape(filename))
+						end
+					end,
+					desc = "Git diff file",
+				},
+				["<leader>gc"] = {
+					function()
+						vim.cmd("Git commit")
+					end,
+					desc = "Git commit",
+				},
+				["<leader>gS"] = {
+					function()
+						vim.cmd("Git status")
+					end,
+					desc = "Git status",
+				},
+			},
 		})
 		vim.cmd([[colorscheme oil]]) -- set the theme
 		vim.cmd([[set background=dark]])
